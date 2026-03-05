c90c7b4e1c127d7e839bef47ea2297ff8bbcaf5e
Nabia Lavala <nabialavala@gmail.com>
2026-03-05 00:58:23 -0500

    Loaded Cards from data base

--- lib/screens/cards_screen.dart
+++ lib/screens/cards_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../repositories/card_repository.dart';
+import '../models/card.dart';
 
 class CardsScreen extends StatefulWidget {
   const CardsScreen({super.key});
@@ -10,6 +11,20 @@ class CardsScreen extends StatefulWidget {
 
 class _CardsScreenState extends State<CardsScreen> {
   final CardRepository _cardRepository = CardRepository();
+  List<PlayingCard> _cards = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCards();
+  }
+
+  Future<void> _loadCards() async{
+    final cards = await _cardRepository.getAllCards();
+    setState(() {
+      _cards = cards;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

e41eadb0453ea57e3c88f142ce6fa307d7f3aae9
Nabia Lavala <nabialavala@gmail.com>
2026-03-05 15:25:47 -0500

    errors in main file addressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:card_organizer_app/screens/cards_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/folders_screen.dart';
-//import 'screens/folders_screen.dart';
 
 void main() {
   runApp(const CardOrganizerApp());
@@ -19,27 +17,7 @@ class CardOrganizerApp extends StatelessWidget {
         useMaterial3: true,
         primarySwatch: Colors.red,
       ),
-      home: const FoldersScreen(),  // ← changed this line
-      //when we're done the final should be 'home: const FoldersScreen()'
-      home: const CardsScreen(), 
-    );
-  }
-}
-
-
-//remove after full app is working
-class PlaceholderHome extends StatelessWidget {
-  const PlaceholderHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Card Organizer'),
-      ),
-      body: const Center(
-        child: Text('Sorry we\'re still working ;).'),
-      ),
+      home: const FoldersScreen(),
     );
   }
 }
